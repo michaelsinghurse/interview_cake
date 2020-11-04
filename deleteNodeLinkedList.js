fa823f6b0b9ebab5736f143c63242fc282d6eb5b
Michael Singhurse <michaelsinghurse@gmail.com>
2020-11-04 12:04:51 -0600

    completed exercise

--- deleteNodeLinkedList.js
+++ deleteNodeLinkedList.js
@@ -1,39 +1,18 @@
-/*
- * Problem
- * - write a function
- * - input: 
- *   - object (instanceof LinkedListNode)
- *   - validate input?
- * - output: 
- *   - boolean for successful?
- * - requirements:
- *   - delete the node passed in - remove it from the list
- *   - should I delete the object or just remove it from the list? remove only 
- *
- * Example
- *  - deleteNode(b);    // true
- *  - a.next === c;     // true
- *  - deleteNode(c);    // true
- *  - a.next === null;  // true
- *
- * Data Structure
- * - none required
- *
- * Algorithm
- * - validate that input is an instance of LinkedListNode. If not return false;
- * - find which node points to "b"
- * - change the value of that node's next property to point to the node b points
- *   to
- * - return true
- */
+// deleteNodeLinkedList.js
 
+// O(1) space
+// O(1) time
+// This method will work from all nodes except the tail. Passing in a tail
+// raises an error. It essentially turns the node passed in into a clone of its
+// next node. It change it's value and next properties to that of that next node
+// in the list. The next node is therefore left dangling.
 function deleteNode(node) {
-  if (!(node instanceof LinkedListNode)) return false;
-  const nextValue = node.next.value;
-  const nextNext = node.next.next;
-  node.value = nextValue;
-  node.next = nextNext;
-  return true;  
+  if (node.next) {
+    node.value = node.next.value;
+    node.next = node.next.next;
+  } else {
+    throw new Error("cannot delete the last node in the list");
+  }
 }
 
 class LinkedListNode {
@@ -43,23 +22,67 @@ class LinkedListNode {
   }
 }
 
-const a = new LinkedListNode("A");
-const b = new LinkedListNode("B");
-const c = new LinkedListNode("C");
+// TESTS
+let desc, actual, expected;
+
+desc = "three nodes in list, delete the head";
+let a = new LinkedListNode("a");
+let b = new LinkedListNode("b");
+let c = new LinkedListNode("c");
 
 a.next = b;
 b.next = c;
 
-/*
+deleteNode(a);
+actual = a.value;
+expected = "b";
+assertAreEqualObjects(desc, actual, expected);
+
+desc = "three nodes in list, delete the middle";
+a = new LinkedListNode("a");
+b = new LinkedListNode("b");
+c = new LinkedListNode("c");
+
+a.next = b;
+b.next = c;
+
 deleteNode(b);
-console.log(a);
-console.log(b);
-console.log(c);
-*/
-// All should return `true`
-console.log(deleteNode({}) === false);
-console.log(deleteNode(b) === true);
-console.log(a.next !== b);
-console.log(a.next.value !== "B");
-console.log(a.next === c);
-console.log(a.next.value === "C");
+actual = a.next.value;
+expected = "c";
+assertAreEqualObjects(desc, actual, expected);
+
+desc = "three nodes in list, delete the tail";
+a = new LinkedListNode("a");
+b = new LinkedListNode("b");
+c = new LinkedListNode("c");
+
+a.next = b;
+b.next = c;
+
+actual = () => deleteNode(c);
+assertThrowsError(desc, actual);
+
+function assertAreEqualObjects(desc, actual, expected) {
+  console.log("------------------------------------------------------------");
+  console.log(desc);
+
+  if (actual === expected) {
+    console.log("PASS");
+  } else {
+    console.log(`FAIL ... ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
+  }
+}
+
+function assertThrowsError(desc, func) {
+  console.log("-------------------------------------------------------------");
+  console.log(desc);
+
+  try {
+    func();
+    console.log("FAIL");
+  } catch (_error) {
+    console.log("PASS");
+  }
+}
+
+
